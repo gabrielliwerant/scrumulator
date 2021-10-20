a7aaea6fece7819d88561f91c48e993bc61e2e75
Gabriel Liwerant <gabriel@abrightconcept.com>
2021-10-19 23:46:10 -0400

    Create basic scrum ordering functionality and remove old

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
+import { reduce as _reduce } from 'lodash';
 
 const useStyles = createUseStyles({
   '@global': {
@@ -10,12 +11,41 @@ const useStyles = createUseStyles({
   }
 });
 
+const PARTICIPANTS = [
+  'Gabriel',
+  'Ada',
+  'Antonio',
+  'Amanda',
+  'Bill',
+  'Carl',
+  'Ciacci'
+];
+
 const App = () => {
   useStyles();
+  const [participants, setParticipants] = useState(PARTICIPANTS);
+  const [current, setCurrent] = useState('');
+
+  const scrumluate = () => {
+    if (!participants.length) return false;
+
+    const index = Math.floor(Math.random() * participants.length);
+
+    setCurrent(participants[index]);
+    setParticipants(_reduce(participants, (acc, cur) => {
+      if (cur !== participants[index]) return [ ...acc, cur ];
+      return [ ...acc ];
+    }, []));
+  };
 
   return (
     <div>
-      Hello World
+      <p>Current: {current}</p>
+      <p>Remaining:</p>
+      <ul>
+        {participants.map(p => <li key={p}>{p}</li>)}
+      </ul>
+      <button onClick={scrumluate}>Go!</button>
     </div>
   );
 };
