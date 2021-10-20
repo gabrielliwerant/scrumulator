ef3c9182695dc24716a1fb0a15dd9fb991a32078
Gabriel Liwerant <gabriel@abrightconcept.com>
2021-10-20 01:21:51 -0400

    Update/refactor ui using material

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import React, { useState } from 'react';
 import { createUseStyles } from 'react-jss';
 import { reduce as _reduce } from 'lodash';
 
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
 const useStyles = createUseStyles({
   '@global': {
     body: {
       margin: 0,
       fontFamily: '"Roboto", arial, sans-serif'
     }
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: '16px'
+  },
+  listContainer: {
+    width: '25%',
+    margin: 'auto'
+  },
+  singleGrid: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  current: {
+    marginTop: '16px'
   }
 });
 
@@ -22,14 +46,17 @@ const PARTICIPANTS = [
 ];
 
 const App = () => {
-  useStyles();
+  const classes = useStyles();
   const [participants, setParticipants] = useState(PARTICIPANTS);
   const [current, setCurrent] = useState('');
+  const [plural, setPlural] = useState(PARTICIPANTS.length ? 's' : '');
 
-  const scrumluate = () => {
+  const scrumuluate = () => {
     if (!participants.length) return false;
 
     const index = Math.floor(Math.random() * participants.length);
+
+    if (participants.length === 2) setPlural('');
 
     setCurrent(participants[index]);
     setParticipants(_reduce(participants, (acc, cur) => {
@@ -40,12 +67,45 @@ const App = () => {
 
   return (
     <div>
-      <p>Current: {current}</p>
-      <p>Remaining:</p>
-      <ul>
-        {participants.map(p => <li key={p}>{p}</li>)}
-      </ul>
-      <button onClick={scrumluate}>Go!</button>
+      <div className={classes.title}>
+        <Typography variant="h3" component="h1">SCRUMULATOR</Typography>
+        <Typography variant="caption">
+          Powered by the scrumulation engine
+        </Typography>
+      </div>
+      <div className={classes.listContainer}>
+        <Paper elevation={4}>
+          <List>
+            <ListItem key='remaining' classNames={classes.singleGrid}>
+              <ListItemText primary={`Remaining Participant${plural}:`} />
+              <Button variant='contained' onClick={scrumuluate}>
+                Scrumulate!
+              </Button>
+            </ListItem>
+            {!!participants.length && <Divider />}
+            {participants.map(p => (
+              <ListItemButton key={p}>
+                <ListItemText primary={p} />
+              </ListItemButton>
+            ))}
+          </List>
+        </Paper>
+        <Paper elevation={4} className={classes.current}>
+          <List>
+            <ListItem key='current'>
+              <ListItemText primary='Current Participant:' />
+            </ListItem>
+            {!!current && (
+              <>
+                <Divider />
+                <ListItem key={current}>
+                  <ListItemText primary={current} />
+                </ListItem>
+              </>
+            )}
+          </List>
+        </Paper>
+      </div>
     </div>
   );
 };
